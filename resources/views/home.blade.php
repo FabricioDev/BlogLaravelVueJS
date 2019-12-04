1568b6abe342631a62232eee024408c66c0ea0f6
Fabrício M. Damasceno <fabricio_wm@hotmail.com>
2019-12-04 16:16:17 -0300

    Componentes VueJS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <example-component></example-component>
+    <pagina tamanho="10">
+        <painel titulo="Dashboard">
+        Teste de Conteúdo...
+
+        <div class="row">
+            <div class="col-md-4">
+                <caixa qtd="80" titulo="Artigos" url="#" cor="orange" icone="ion ion-pie-graph" ></caixa>
+            </div>
+
+            <div class="col-md-4">
+            <caixa qtd="1500" titulo="Usuários" url="#" cor="blue" icone="ion ion-person-stalker" ></caixa>
+            </div>
+            
+            <div class="col-md-4">
+            <caixa qtd="3" titulo="Autores" url="#" cor="green" icone="ion ion-person" ></caixa>
+            </div>
+        </div>
+
+        </painel>
+    </pagina>
 @endsection
